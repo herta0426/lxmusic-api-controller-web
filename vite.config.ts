966428f1644b845033488e60160e43dcb8e58638
Herta <118590287+herta0426@users.noreply.github.com>
2025-06-07 06:41:04 +0800

    fix: set vite base for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/lxmusic-api-controller-web/', // 添加这一行，注意斜杠和仓库名
   plugins: [react()],
 })
